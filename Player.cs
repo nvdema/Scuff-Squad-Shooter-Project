45f1f470d72e8a30ee62d0287c4acfdb1096a31a
PieMaster87 <168610468+PieMaster87@users.noreply.github.com>
2025-11-03 14:40:19 -0500

    Player border shenanigans

---
+++ Player.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //how to define a variable
+    //1. access modifier: public or private
+    //2. data type: int, float, bool, string
+    //3. variable name: camelCase
+    //4. value: optional
+
+    private float playerSpeed;
+    private float horizontalInput;
+    private float verticalInput;
+
+    private float horizontalScreenLimit = 9.5f;
+    private float verticalScreenLimit = 3.5f;
+    private float lowerverticalScreenLimit = -3.5f;
+
+    public GameObject bulletPrefab;
+
+    void Start()
+    {
+        playerSpeed = 6f;
+        //This function is called at the start of the game
+        
+    }
+
+    void Update()
+    {
+        //This function is called every frame; 60 frames/second
+        Movement();
+        Shooting();
+
+    }
+
+    void Shooting()
+    {
+        //if the player presses the SPACE key, create a projectile
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(bulletPrefab, transform.position + new Vector3(0, 1, 0), Quaternion.identity);
+        }
+    }
+
+    void Movement()
+    {
+        //Read the input from the player
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        //Move the player
+        transform.Translate(new Vector3(horizontalInput, verticalInput, 0) * Time.deltaTime * playerSpeed);
+        //Player leaves the screen horizontally
+        if(transform.position.x > horizontalScreenLimit || transform.position.x <= -horizontalScreenLimit)
+        {
+            transform.position = new Vector3(transform.position.x * -1, transform.position.y, 0);
+        }
+       
+        //Player ATTEMPTS to leave the screen through the Top
+        if (transform.position.y > verticalScreenLimit)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y - 0.02f);
+        }
+        //Bottom half of screen code
+        float bottomLimit = -verticalScreenLimit;
+        float topLimit = 0;
+        transform.position = new Vector3(
+            transform.position.x,
+            Mathf.Clamp(transform.position.y, bottomLimit, topLimit),
+            transform.position.z
+            );
+    }
+
+}
+
